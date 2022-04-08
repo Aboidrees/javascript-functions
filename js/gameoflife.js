55dcd02df9b9c357751cf94f2e30a74d6a8bf495
Muhammad Yousif <m.alshf3ee@gmail.com>
2022-04-08 12:38:13 +0300

    Will Be Alive Function

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -68,7 +68,14 @@ const getLivingNeighbors = (cell, state) => {
   return getNeighborsOf(cell).filter((neighbor) => contains.call(state, neighbor));
 };
 
-const willBeAlive = (cell, state) => {};
+const willBeAlive = (cell, state) => {
+  const livingNeighbors = getLivingNeighbors(cell, state);
+
+  return (
+    livingNeighbors.length === 3 ||
+    (contains.call(state, cell) && livingNeighbors.length === 2)
+  )
+};
 
 const calculateNext = (state) => {};
 
